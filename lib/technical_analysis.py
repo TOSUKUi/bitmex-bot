f96f65b3694f7a1ca22fa47bfa1782e1dd31e695
TOSUKUi <am.businessmail.exp@gmail.com>
2018-04-13 16:53:33 +0900

    add wvf logic

--- lib/technical_analysis.py
+++ lib/technical_analysis.py
@@ -2,33 +2,36 @@ import pandas as pd
 
 
 def rci3lines(long_itv, mid_itv, short_itv, bins):
-    close = bin["close"]
+    close = bins["close"]
     long = __rci(long_itv, close)
     mid = __rci(mid_itv, close)
     short = __rci(short_itv, close)
     return long, mid, short
 
 
-def williams_vix_fix(bin):
-    wvf = ""
+def williams_vix_fix(bins, period):
+    wvf = ((__highest(bins["close"], period)-bins.loc[-1]["low"])/(__highest(bins["close"], period)))
     return wvf
 
 
 def __ord(seq, idx, itv):
     p = seq[idx]
     o = 1
-    for i in range(itv - 1):
+    for i in range(0, itv):
         if p < seq[i]:
             o = o + 1
     return o
 
 
 def __d(itv, src):
-    sum = 0.0
-    for i in range(itv - 1):
-        sum = sum + pow((i + 1) - __ord(src, i, itv), 2)
-    sum
+    total = 0.0
+    for i in range(0, itv):
+        total += pow((i + 1) - __ord(src, i, itv), 2)
+    return total
 
+
+def __highest(src, span):
+    return max(src[:span])
 
 def __rci(itv, src):
     return (1.0 - 6.0 * __d(itv, src) / (itv * (itv * itv - 1.0))) * 100.0 + 100
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from logic import vix_and_rci
 from lib.bitmex_api_connecter import test_connection
 import pandas as pd
+import lib.technical_analysis as ta
 
 if __name__ == "__main__":
     bitmex = test_connection()
@@ -9,5 +10,6 @@ if __name__ == "__main__":
     since = bitmex.milliseconds() - limit * 300 * 500
     bins = bitmex.fetch_ohlcv('BTC/USD', timeframe="5m", since=since, limit=limit)
     df = pd.DataFrame(bins)
+    ta.williams_vix_fix(df, 9)
 
     print(df)

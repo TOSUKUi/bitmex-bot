036206a08c79244bdc51faba05db74843adfc94a
TOSUKUi <am.businessmail.exp@gmail.com>
2018-06-28 23:22:06 +0900

    add order logic

--- lib/order.py
+++ lib/order.py
@@ -1,26 +1,43 @@
 import ccxt
 import lib.bitmex_api_connecter as connector
 import pandas as pd
+from datetime import datetime
 
 class Order():
 
     def __init__(self, bot_id, symbol="BTC/USD", test=False):
         self.client = None
         if test == "back_test":
-            client = None
+            self.client = None
         elif test == "test":
-            client = connector.test_connection(bot_id)
+            self.client = connector.test_connection(bot_id)
         elif test is None:
-            client = connector.main_connection(bot_id)
+            self.client = connector.main_connection(bot_id)
         self.test = test
         self.symbol = symbol
         self.bot_id = bot_id
         self.trading_file = "{bot_id}_trading_file.csv".format(bot_id=bot_id)
 
-    def create_buy_order(self, amount, id):
+    def create_order(self, amount, side, price=None):
         df = pd.read_csv(self.trading_file)
         positions = df[df["is_closed"] == 0]
+        result = {}
+        position = {}
         if len(positions) < 1:
+            if self.client is not None:
+                if price is not None:
+                    result = self.client.create_limit_buy_order("BTC/USD", amount=amount, price=price)
+                else:
+                    result = self.client.create_market_buy_order("BTC/USD", amount=amount)
+                position["entry_price"] = result["price"]
+                position["is_test"] = 0
+            else:
+                position["entry_price"] = price
+                position["is_test"] = 1
+
+            position = {}
+            position["closed"] = 0
+            position["entried_at"] = datetime.now()
 
             df.append([])
 

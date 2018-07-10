c4d901a1b56e2d5ac3f74ac1670bd75135fc02f0
TOSUKUi <am.businessmail.exp@gmail.com>
2018-07-10 17:04:07 +0900

    add order and api credential yml file

--- lib/order.py
+++ lib/order.py
@@ -4,41 +4,31 @@ import pandas as pd
 from datetime import datetime
 
 class Order():
-
     def __init__(self, bot_id, symbol="BTC/USD", test=False):
         self.client = None
-        if test == "back_test":
-            self.client = None
-        elif test == "test":
+        if test:
             self.client = connector.test_connection(bot_id)
-        elif test is None:
+        else:
             self.client = connector.main_connection(bot_id)
         self.test = test
         self.symbol = symbol
         self.bot_id = bot_id
-        self.trading_file = "trading_data/{bot_id}_trading_file.csv".format(bot_id=bot_id)
+        self.trading_file = "trading_data/{bot_id}_trading_logs.csv".format(bot_id=bot_id)
 
-    def create_order(self, amount, side, price=None):
-        df = pd.read_csv(self.trading_file)
-
+    def send_order(self, amount, side, price=None):
+        try:
+            df = pd.read_csv(self.trading_file)
+        except:
+            df = pd.DataFrame()
         result = {}
-        position = {}
-        if len(positions) < 1:
-            if self.client is not None:
-                if price is not None:
-                    result = self.client.create_limit_order("BTC/USD", side=side, amount=amount, price=price)
-                else:
-                    result = self.client.create_market_order("BTC/USD", side=side, amount=amount)
-                del result["info"]
-                positions.append()
+        if price is not None:
+            result = self.client.create_limit_order("BTC/USD", side=side, amount=amount, price=price)
+        else:
+            result = self.client.create_market_order("BTC/USD", side=side, amount=amount)
+        del result["info"]
+        df.append(result)
 
-    def close_order(self, id):
-
-
-
-
-
-
+    #def close_position(self, id):
     def create_stop_order(self, amount, id):
         params = {'type': 'Stop', "stopPx": -50}
 
